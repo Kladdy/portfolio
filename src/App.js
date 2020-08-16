8ce84fd9287c677ff7435aadb437f6ea92f50778
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2020-08-17 00:45:10 +0200

    Add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './components/sidebar'
+import About from './components/about'
+import Experience from './components/experience';
+import Education from './components/education';
+import Skills from './components/skills';
+import Interests from './components/interests';
+import Awards from './components/awards';
 
 function App() {
   return (
-    <Sidebar />
+    <div id="parent">
+      <Sidebar></Sidebar>
+      <div className="container-fluid p-0">
+        <About></About>
+        <Experience></Experience>
+        <Education></Education>
+        <Skills></Skills>
+        <Interests></Interests>
+        <Awards></Awards>
+      </div>
+    </div>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
