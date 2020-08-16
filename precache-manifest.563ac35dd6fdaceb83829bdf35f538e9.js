6ce2baf4ebbb2669b6078ca1e970952104c73cd7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-16 22:46:26 +0000

    Deploy Kladdy/portfolio to github.com/Kladdy/portfolio.git:gh-pages

---
+++ precache-manifest.563ac35dd6fdaceb83829bdf35f538e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d48f77fed5dead26dec6da60063183b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e47fe563fc70677f552e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb0e86f4193336d1f4ce",
+    "url": "/static/js/2.55c92afe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.55c92afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47fe563fc70677f552e",
+    "url": "/static/js/main.7600f71b.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
